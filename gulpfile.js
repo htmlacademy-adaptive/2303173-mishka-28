5709c26a9b6d64018d4bbbe552d8c8d7f507ec0e
Keks <keks@htmlacademy.ru>
2023-04-17 13:27:39 +0300

    :cat:

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,45 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
 
-gulp.task('sass-compile', function(){
-    return gulp.src('./source/sass/**/*.scss')
+// Styles
+
+export const styles = () => {
+  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+    .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./source/css/'))
-})
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
 
-gulp.task('watch', function(){
-    gulp.watch('./source/sass/**/*.scss', gulp.series('sass-compile'))
-})
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/*.html').on('change', browser.reload);
+}
 
 
+export default gulp.series(
+  styles, server, watcher
+);
